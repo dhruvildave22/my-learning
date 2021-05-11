c00c24bd286471e0497b3991fe0a605e3cbb642d
Dhruvil Dave <dhruvildave22@gmail.com>
2021-05-11 15:40:51 +0530

    Add css and js file

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,22 +1,18 @@
-const { request } = require("express")
+const path = require('path')
 const express = require("express")
 const app = express()
 
+const publicDirectoryPath = path.join(__dirname, '../public')
 
-app.get('', (req, res) => {
-  res.send("Hello World")
-})
+app.use(express.static(publicDirectoryPath))
 
-app.get('/help',(req, res) => {
-  res.send("Help page")
-})
 
-app.get('/about', (req, res) => {
-  res.send('About')
-})
 
 app.get('/weather', (req, res) => {
-  res.send('Your weather')
+  res.send({
+    forecast: 'It is snowing',
+    location: 'Philadelphia'
+  })
 })
 
 
