ec7cadb32fac2834145af3d0f5443d2fe680d98e
Dhruvil Dave <dhruvildave22@gmail.com>
2021-05-11 17:35:05 +0530

    404 Pages added

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,14 +1,19 @@
 const path = require('path')
 const express = require("express")
+const hbs = require('hbs')
+
 const app = express()
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
-const viewsPath = path.join(__dirname, '../templates')
+const viewsPath = path.join(__dirname, '../templates/views')
+const partialsPath = path.join(__dirname, '../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine', 'hbs')
 app.set('views', viewsPath)
+hbs.registerPartials(partialsPath)
+
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
@@ -30,7 +35,9 @@ app.get('/about', (req, res) => {
 
 app.get('/help', (req, res) => {
   res.render('help', {
-    helpText: 'This is some helpful text.'
+    helpText: 'This is some helpful text.',
+    title: 'Help',
+    name: 'Dhruvil Dave'
   })
 })
 
@@ -43,6 +50,21 @@ app.get('/weather', (req, res) => {
   })
 })
 
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Dhruvil Dave',
+    errorMessage: 'Help article not found.'
+  })
+})
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Dhruvil Dave',
+    errorMessage: 'Page not found.'
+  })
+})
 
 app.listen(3000, () => {
   console.log('Server is up on port 3000.')
