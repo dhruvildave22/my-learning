765dd3c85f5fed0dc2392bd4d07822f2ded06aa5
Dhruvil Dave <dhruvildave22@gmail.com>
2021-05-11 15:15:33 +0530

    Hello Express

---
+++ web-server/src/app.js
@@ -0,0 +1,25 @@
+const { request } = require("express")
+const express = require("express")
+const app = express()
+
+
+app.get('', (req, res) => {
+  res.send("Hello World")
+})
+
+app.get('/help',(req, res) => {
+  res.send("Help page")
+})
+
+app.get('/about', (req, res) => {
+  res.send('About')
+})
+
+app.get('/weather', (req, res) => {
+  res.send('Your weather')
+})
+
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000.')
+})
