f99c4364153e7c61b66c08946865c901567ff3e8
Dhruvil Dave <dhruvildave22@gmail.com>
2021-05-13 18:10:53 +0530

    Paginating Data

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -18,6 +18,8 @@ router.post('/tasks', auth, async (req, res) => {
   }
 })
 
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=20
 router.get('/tasks', auth, async (req, res) => {
   const match = {}
 
@@ -25,7 +27,14 @@ router.get('/tasks', auth, async (req, res) => {
     match.completed = req.query.completed === 'true'
   }
   try {
-    await req.user.populate({ path: 'tasks', match }).execPopulate()
+    await req.user.populate({
+      path: 'tasks',
+      match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip)
+      }
+    }).execPopulate()
     res.send(req.user.tasks)
   } catch (e) {
     res.status(500).send()
