d1acc573dc9f040273f1575b0d514edb5c197795
Dhruvil Dave <dhruvildave22@gmail.com>
2021-05-13 18:18:37 +0530

    Sorting Data

--- task-manager/src/routers/task.js
+++ task-manager/src/routers/task.js
@@ -20,11 +20,19 @@ router.post('/tasks', auth, async (req, res) => {
 
 // GET /tasks?completed=true
 // GET /tasks?limit=10&skip=20
+// GET /tasks?sortBy=createdAt:desc
+
 router.get('/tasks', auth, async (req, res) => {
   const match = {}
+  const sort = {}
 
   if (req.query.completed) {
     match.completed = req.query.completed === 'true'
+  }
+
+  if (req.query.sortBy) {
+    const part = req.query.sortBy.split(':')
+    sort[part[0]] = part[1] === 'desc' ? -1 : 1
   }
   try {
     await req.user.populate({
@@ -32,7 +40,8 @@ router.get('/tasks', auth, async (req, res) => {
       match,
       options: {
         limit: parseInt(req.query.limit),
-        skip: parseInt(req.query.skip)
+        skip: parseInt(req.query.skip),
+        sort
       }
     }).execPopulate()
     res.send(req.user.tasks)
