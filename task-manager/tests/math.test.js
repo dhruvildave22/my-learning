a0f11b569063498ba938088701ab0a8d9b546773
Dhruvil Dave <dhruvildave22@gmail.com>
2021-05-13 20:37:08 +0530

    Jest Testing Framework

---
+++ task-manager/tests/math.test.js
@@ -0,0 +1,18 @@
+test('Hello World!', () => {
+
+})
+
+test('This should fail', () => {
+  throw new Error('Failure!')
+})
+
+// 
+// Why test?
+// 
+// - Saves time
+// - Creates reliable software
+// - Gives flexibility to developers
+//   - Refactoring
+//   - Collaborating
+//   - Profiling
+// - Peace of mind
