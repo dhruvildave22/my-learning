fca9466711ddbbf1f186cac631e81d800844ecda
Dhruvil Dave <dhruvildave22@gmail.com>
2021-05-14 12:54:20 +0530

    Wrapping up User Tests

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -81,8 +81,8 @@ test('Should delete account for user', async () => {
     .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
     .send()
     .expect(200)
-    const user = await User.findById(userOneId)
-    expect(user).toBeNull()
+  const user = await User.findById(userOneId)
+  expect(user).toBeNull()
 })
 
 test('Should get not delete account for unauthenticated user', async () => {
@@ -90,4 +90,37 @@ test('Should get not delete account for unauthenticated user', async () => {
     .delete('/users/me')
     .send()
     .expect(401)
+})
+
+test('Should upload avatar image', async () => {
+
+  await request(app)
+    .post('/users/me/avatar')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .attach('avatar', 'tests/fixtures/profile-pic.jpg')
+    .expect(200)
+  const user = await User.findById(userOneId)
+  expect(user.avatar).toEqual(expect.any(Buffer))
+})
+
+test('Should update valid user fields', async () => {
+  await request(app)
+    .patch('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      name: 'Dhruvil'
+    })
+    .expect(200)
+  const user = await User.findById(userOneId)
+  expect(user.name).toEqual('Dhruvil')
+})
+
+test('Should not update invalid user fields', async () => {
+  await request(app)
+    .patch('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send({
+      location: 'Philadelphia'
+    })
+    .expect(400)
 })
