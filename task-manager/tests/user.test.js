5947739b4538253763961927db64f555849f5762
Dhruvil Dave <dhruvildave22@gmail.com>
2021-05-14 12:03:58 +0530

    Jest Setup and Teardown

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,5 +1,17 @@
 const request = require('supertest')
 const app = require('../src/app')
+const User = require('../src/models/user')
+
+const userOne = {
+  name: 'Mike',
+  email: 'mike@mailinator.com',
+  password: '56what!!'
+}
+
+beforeEach(async () => {
+  await User.deleteMany()
+  await new User(userOne).save()
+})
 
 test('Should signup a new user', async () => {
   await request(app).post('/users').send({
@@ -8,3 +20,17 @@ test('Should signup a new user', async () => {
     password: "Abcd@1234"
   }).expect(201)
 })  
+
+test('Should login to existing user', async () => {
+  await request(app).post('/users/login').send({
+    email: userOne.email,
+    password: userOne.password
+  }).expect(200)
+})
+
+test('Should not login to nonexisting user', async ( )=> {
+  await request(app).post('/users/login').send({
+    email: userOne.email,
+    password: 'wrongpassword'
+  }).expect(400)
+})
