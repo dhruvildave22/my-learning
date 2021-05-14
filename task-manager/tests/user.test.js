c0a6e99032215b32b71aa7508efad3d71a612b0f
Dhruvil Dave <dhruvildave22@gmail.com>
2021-05-14 12:15:50 +0530

    Testing with Authentication

--- task-manager/tests/user.test.js
+++ task-manager/tests/user.test.js
@@ -1,11 +1,19 @@
 const request = require('supertest')
+const jwt = require('jsonwebtoken')
+const mongoose = require('mongoose')
 const app = require('../src/app')
 const User = require('../src/models/user')
 
+const userOneId = new mongoose.Types.ObjectId()
+
 const userOne = {
+  _id: userOneId,
   name: 'Mike',
   email: 'mike@mailinator.com',
-  password: '56what!!'
+  password: '56what!!',
+  tokens: [{
+    token: jwt.sign({ _id: userOneId }, process.env.JWT_SECRET)
+  }]
 }
 
 beforeEach(async () => {
@@ -19,7 +27,7 @@ test('Should signup a new user', async () => {
     email: "dhruvildave22@mailinator.com",
     password: "Abcd@1234"
   }).expect(201)
-})  
+})
 
 test('Should login to existing user', async () => {
   await request(app).post('/users/login').send({
@@ -28,9 +36,40 @@ test('Should login to existing user', async () => {
   }).expect(200)
 })
 
-test('Should not login to nonexisting user', async ( )=> {
+test('Should not login to nonexisting user', async () => {
   await request(app).post('/users/login').send({
     email: userOne.email,
     password: 'wrongpassword'
   }).expect(400)
+})
+
+test('Should get profile for loggin user', async () => {
+  await request(app)
+    .get('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200)
+})
+
+test('Should get not profile for unauthenticated user', async () => {
+  await request(app)
+    .get('/users/me')
+    .send()
+    .expect(401)
+})
+
+
+test('Should delete account for user', async () => {
+  await request(app)
+    .delete('/users/me')
+    .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
+    .send()
+    .expect(200)
+})
+
+test('Should get not delete account for unauthenticated user', async () => {
+  await request(app)
+    .delete('/users/me')
+    .send()
+    .expect(401)
 })
